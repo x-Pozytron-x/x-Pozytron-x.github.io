b1f26447ccb2de439c434b6f0b54f742a7c6ae37
x_pozytron_x <stetsenko.vitaliy@gmail.com>
2018-12-27 13:54:17 +0200

    fix menu

--- menu.js
+++ menu.js
@@ -1,10 +1,12 @@
 var menu =
   '<li><a href="/">На главную</a></li>' +
+  '<ul>' +
   '<li><a href="/myLib/tinySlider-byPozytron">TinySlider (by Pozytron) - легкий (2.74KB) и  простой слайдер без jquery</a></li>' +
   '<li><a href="/myLib/Color-Generator">Color-Generator</a></li>' +
   '<li><a href="/myLib/octagon">Шестиугольник средствами html+css</a></li>' +
   '<li><a href="/myLib/RangeSlider">Ползунок-(калькулятор) на bootstrap-js</a></li>' +
-  '<li><a href="myProjects/SolarSystem">Солнечная система v.0.2.1</a></li>';
+  '<li><a href="/myProjects/SolarSystem">Солнечная система v.0.2.1</a></li>' +
+  '</ul>';
 window.onload = function() {
   var ol = document.getElementById('ol');
   ol.innerHTML = menu;
