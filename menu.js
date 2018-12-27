4ced8391e00cddd6f92d527b99e1467c020a2cee
x_pozytron_x <stetsenko.vitaliy@gmail.com>
2018-12-27 13:50:46 +0200

    add menu

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 var menu =
   '<li><a href="/">На главную</a></li>' +
-  '<lh>Мои наработки</lh>' +
   '<li><a href="/myLib/tinySlider-byPozytron">TinySlider (by Pozytron) - легкий (2.74KB) и  простой слайдер без jquery</a></li>' +
   '<li><a href="/myLib/Color-Generator">Color-Generator</a></li>' +
   '<li><a href="/myLib/octagon">Шестиугольник средствами html+css</a></li>' +
-  '<li><a href="/myLib/RangeSlider">Ползунок-(калькулятор) на bootstrap-js</a></li>';
+  '<li><a href="/myLib/RangeSlider">Ползунок-(калькулятор) на bootstrap-js</a></li>' +
+  '<li><a href="myProjects/SolarSystem">Солнечная система v.0.2.1</a></li>';
 window.onload = function() {
   var ol = document.getElementById('ol');
   ol.innerHTML = menu;
