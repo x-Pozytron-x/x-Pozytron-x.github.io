7b74914e318d6d06fccda810c22b2f9d41b1e793
x-Pozytron-x <stetsenko.vitaliy@gmail.com>
2019-07-04 12:01:43 +0300

    Update script.js

--- myLib/tinySlider-byPozytron/script.js
+++ myLib/tinySlider-byPozytron/script.js
@@ -1,19 +1,30 @@
 function tinySlider(options){
-    var slider   = document.querySelector('#slider');
+    var slider   = document.querySelector('#tinySlider');
     var sliderWrap  = slider.querySelector('.sliderWrap');
     var arrLeft  = slider.querySelector('.arrLeft');
     var arrRight = slider.querySelector('.arrRight');
     var items    = slider.getElementsByClassName('slideItem');
     var currentMarginLeft = 0;
-
-    slider.style.height = options.height + options.units; 
-    slider.style.width = options.width + options.units;  
-    sliderWrap.style.height = options.height + options.units;
-    sliderWrap.style.width = options.width * items.length  + options.units;
-    slider.style.display = 'block';
-
+    var currentItem = 0;
+    
+    slider.style.height = options.height + 'px'; 
+    slider.style.width = options.width + 'px';  
+    sliderWrap.style.height = options.height + 'px';
+    sliderWrap.style.width = options.width * items.length  + 'px';
     arrRight.style.display = 'none';
     
+    items[0].classList.add('active');
+    
+    
+    if(options.arrows == '0') {
+         noArrows(); 
+    } 
+    if(options.autoscroll == '1') {
+        setInterval(function(){
+            slideLeft();  
+            return currentMarginLeft;
+        }, 4000); 
+    }
     arrLeft.onclick  = function () { 
         slideLeft();  
         return currentMarginLeft;
@@ -22,25 +33,63 @@ function tinySlider(options){
         slideRight();  
         return currentMarginLeft;
     }
+    
+    function noArrows() {
+       arrRight.style.display = 'none';
+       arrLeft.style.display = 'none';
+    } 
+    
     function slideLeft () {
-        arrRight.style.display = 'block';
-        newMarginLeft = parseInt(currentMarginLeft) - options.width + options.units;   
-        sliderWrap.style.marginLeft = newMarginLeft; 
-        currentMarginLeft = newMarginLeft; 
-        if (currentMarginLeft == (options.width * (items.length - 1) * -1 + options.units)) {
-            arrLeft.style.display = 'none';
+        var activeItem  = slider.querySelector('.active');
+        activeItem.classList.remove('active');
+        currentItem++;
+        if(currentItem == items.length) {
+            items[0].classList.add('active');
+            currentMarginLeft = 0;
+            currentItem = 0;
+            sliderWrap.style.marginLeft = currentMarginLeft;
+        } else {
+            newMarginLeft = parseInt(currentMarginLeft) - options.width + 'px';   
+            sliderWrap.style.marginLeft = newMarginLeft; 
+            currentMarginLeft = newMarginLeft; 
+        }
+        items[currentItem].classList.add('active');
+        
+        if(options.arrows == '0') {
+            noArrows();
+        } else {
+            arrRight.style.display = 'block';
+            if (currentMarginLeft == (options.width * (items.length - 1) * -1 + 'px')) {
+                arrLeft.style.display = 'none';
+            }
         }
         return currentMarginLeft;
     }
     function slideRight () { 
-        arrLeft.style.display = 'block';
-        newMarginLeft = parseInt(currentMarginLeft) + options.width + options.units;   
-        sliderWrap.style.marginLeft = newMarginLeft;
-        currentMarginLeft = parseInt(newMarginLeft); 
-        if (currentMarginLeft == '0') {
-            arrRight.style.display = 'none';
+        
+        var activeItem  = slider.querySelector('.active');
+        activeItem.classList.remove('active');
+        currentItem--;
+        if(currentItem < 0) {
+            var last = items.length - 1;
+            items[last].classList.add('active');
+            currentMarginLeft = options.width * items.length + 'px';
+            currentItem = items[last];
+            sliderWrap.style.marginLeft = currentMarginLeft;
         } else {
-            currentMarginLeft = currentMarginLeft + options.units;
+            newMarginLeft = parseInt(currentMarginLeft) + options.width + 'px';   
+            sliderWrap.style.marginLeft = newMarginLeft;
+            currentMarginLeft = newMarginLeft; 
+        }
+        items[currentItem].classList.add('active');
+        
+        if(options.arrows == '0') {
+            noArrows();
+        } else {
+            arrLeft.style.display = 'block';
+            if (currentMarginLeft == '0px') {
+                arrRight.style.display = 'none';
+            }
         }
         return currentMarginLeft;
     }
